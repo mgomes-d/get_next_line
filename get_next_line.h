5576a52935980cc8189831dd4ba132ca26bd02da
Murillo Gomes De Senna <mgomes-d@shi-r10-p1.s19.be>
2022-11-04 16:02:29 +0100

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgomes-d <mgomes-d@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/21 15:47:35 by mgomes-d          #+#    #+#             */
+/*   Updated: 2022/11/04 12:20:38 by mgomes-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+int		ft_not_nl_static(char *str);
+char	*ft_strjoingnl(char *str1, char *str2);
+char	*ft_free_all(char **str1);
+
+#endif
